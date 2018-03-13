8355aa182f87b32dac70df4c12ce474dec1859ac
Lee Jaebeom <woqja125@gmail.com>
2018-03-14 07:03:19 +0900

    fix errors

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -5,30 +5,29 @@
 #include <linux/sched.h>
 #include <linux/list.h>
 #include <linux/string.h>
+#include <linux/slab.h>
 
-static int ptree_dfs(struct task_struct *now, struct prinfo** out, struct prinfo* end, pid_t next_sibling, pid_t par) {
+static int ptree_dfs(struct task_struct *now, struct prinfo* out, int *idx, int max_len, pid_t next_sibling, pid_t par) {
 	int re = 0, err;
 	struct list_head *p;
 	struct task_struct *t;
-	struct prinfo info;
 	pid_t ns = 0;
 	printk("[ptree] %s,%d\n", now->comm, now->pid);
 	
 	if (now != &init_task){
 		re = 1;
-		strcpy(info.comm, now->comm);
-		if (list_empty(&now->children)) info.first_child_pid = 0;
-		else info.first_child_pid = list_entry(now->children.next, struct task_struct, sibling)->pid;
-		info.next_sibling_pid = next_sibling;
-		info.parent_pid = par;
-		info.pid = now->pid;
-		info.state = now->state;
-		info.uid = task_uid(now);
-		if(*out != end){
-			err = copy_to_user(&info, *out, sizeof(struct prinfo));
-			if(err < 0) return err;
-			(*out)++;
+		if(*idx < max_len){
+			strcpy(out[*idx].comm, now->comm);
+			if (list_empty(&now->children)) out[*idx].first_child_pid = 0;
+			else out[*idx].first_child_pid = list_entry(now->children.next, struct task_struct, sibling)->pid;
+			out[*idx].next_sibling_pid = next_sibling;
+			out[*idx].parent_pid = par;
+			out[*idx].pid = now->pid;
+			out[*idx].state = now->state;
+			out[*idx].uid = task_uid(now);
+			(*idx)++;
 		}
+		else printk("[ptree]    end of buffer\n");
 	}
 
 	list_for_each(p, &now->children) {
@@ -37,7 +36,9 @@ static int ptree_dfs(struct task_struct *now, struct prinfo** out, struct prinfo
 		if(p->next != &(now->children)) {
 			ns = list_entry(p->next, struct task_struct, sibling)->pid;
 		}
-		re += ptree_dfs(t, out, end, ns, now->pid);
+		err = ptree_dfs(t, out, idx, max_len, ns, now->pid);
+		if(err < 0) return err;
+		re += err;
 	}
 	return re;
 }
@@ -48,6 +49,9 @@ SYSCALL_DEFINE2(ptree, struct prinfo *, buf, int *, nr)
 	int list_size;
 	int err = 0;
 	int num_of_proc;
+	int remain_bytes, write_bytes;
+	void *kbuf, *ubuf, *kbuf_origin;
+	int idx = 0;
 
 	if (buf == NULL || nr == NULL) return -EINVAL;
 
@@ -56,10 +60,26 @@ SYSCALL_DEFINE2(ptree, struct prinfo *, buf, int *, nr)
 
 	if (access_ok(VERIFY_WRITE, buf, list_size * sizeof(struct prinfo)) < 0) return -EFAULT;
 
+	kbuf_origin = kbuf = kmalloc(list_size*sizeof(struct prinfo), GFP_KERNEL);
+
 	read_lock(&tasklist_lock);
-	num_of_proc = ptree_dfs(&init_task, &buf, buf + list_size, -1, -1);
+	num_of_proc = ptree_dfs(&init_task, kbuf, &idx, list_size, -1, -1);
 	read_unlock(&tasklist_lock);
 
 	if (num_of_proc < 0) return num_of_proc;
-	return num_of_proc - 1; 
+
+	ubuf = buf;
+	remain_bytes = (list_size<num_of_proc?list_size:num_of_proc) * sizeof(struct prinfo);
+	while(remain_bytes > 0){
+		write_bytes = copy_to_user(ubuf, kbuf, remain_bytes);
+		if(write_bytes < 0) return write_bytes;
+		write_bytes = remain_bytes - write_bytes;
+		ubuf += write_bytes;
+		kbuf += write_bytes;
+		remain_bytes -= write_bytes;
+	}
+
+	kfree(kbuf_origin);
+
+	return num_of_proc;
 }
