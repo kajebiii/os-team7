c3d549ef9e8a6dfb7c4ce7b96a211dc396071505
Lee Jaebeom <woqja125@gmail.com>
2018-03-14 07:03:36 +0900

    test program

--- testcode/test.c
+++ testcode/test.c
@@ -1,13 +1,37 @@
 #include <asm/unistd.h>
 #include <errno.h>
 #include <stdio.h>
+#include <memory.h>
+#include <linux/prinfo.h>
 
 int main() {
-	int temp = -1;
-	int a = 2016, b = 17101;
-	temp = syscall(380, &a, &b);
+	struct prinfo *data = NULL;
+	int size = 500, re, i;
+	int *stk, top=-1;
+	while(true){
+//		printf("I will malloc !!!\n");		
+		data = (struct prinfo*)malloc(size * sizeof(struct prinfo));
+		re = syscall(380, data, &size);
 
-	printf("temp : %d\n", temp);
-	
+//		printf("^^Hi %d %d\n", size, re);
+//	for(i=0; i < re && i < size; i++){
+//		printf("^^%*s%s,%d,%ld,%d,%d,%d,%d\n", 0, "", data[i].comm, data[i].pid,
+//			data[i].state,data[i].parent_pid, data[i].first_child_pid, data[i].next_sibling_pid, data[i].uid);
+//	}
+
+//		printf("^^Hello %d %d\n", size, re);
+		if(re <= size) break;
+		size = re + 10;
+		free(data);
+	}
+	stk = malloc((re + 10) * sizeof(int));
+	for(i=0; i<re; i++){
+		while(top != -1 && stk[top] != data[i].parent_pid) top--;
+		stk[++top] = data[i].pid;
+		printf("%*s%s,%d,%ld,%d,%d,%d,%d\n", top*2, "", data[i].comm, data[i].pid,
+			data[i].state,data[i].parent_pid, data[i].first_child_pid, data[i].next_sibling_pid, data[i].uid);
+	}
+	free(data);
+	free(stk);
 	return 0;
 }
