3bb99446ae92ba0107b797f537629bb9a58c9d5b
JongBeom Kim <kajebiii@gmail.com>
2018-03-14 02:06:51 +0900

    implement dfs in ptree.c

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -3,11 +3,28 @@
 #include <linux/prinfo.h>
 #include <linux/syscalls.h>
 #include <linux/sched.h>
+#include <linux/list.h>
 
+void dfs(struct task_struct *now, int dep) {
+	printk("[ptree] %s,%d\n", now->comm, now->pid);
+	struct list_head *p;
+	struct task_struct *t;
+	/*
+	list_for_each_entry(t, &now->children, children) {
+		printk("[ptree] %s,%d\n", t->comm, t->pid);
+	}
+	*/
+	list_for_each(p, &now->children) {
+		t = list_entry(p, struct task_struct, sibling);
+		//if(dep == 0) dfs(t, dep+1);
+		if(t != NULL) dfs(t, dep+1);
+	}
+}
 SYSCALL_DEFINE2(ptree, struct prinfo *, buf, int *, nr)
 {
-	struct task_struct root = init_task;
-	printk("[ptree] %d\n", root.pid);
+	struct task_struct *root = &init_task;
+	printk("[ptree] %d\n", root->pid);
 	printk("[ptree] hello, world!\n");
+	dfs(root, 0);
 	return 1;
 }
