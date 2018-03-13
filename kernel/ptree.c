40cfb379289a824a603f729679e59593f7f03b7c
Lee Jaebeom <woqja125@gmail.com>
2018-03-14 04:36:58 +0900

    Implement ptree syscall

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -4,27 +4,62 @@
 #include <linux/syscalls.h>
 #include <linux/sched.h>
 #include <linux/list.h>
+#include <linux/string.h>
 
-void dfs(struct task_struct *now, int dep) {
-	printk("[ptree] %s,%d\n", now->comm, now->pid);
+static int ptree_dfs(struct task_struct *now, struct prinfo** out, struct prinfo* end, pid_t next_sibling, pid_t par) {
+	int re = 0, err;
 	struct list_head *p;
 	struct task_struct *t;
-	/*
-	list_for_each_entry(t, &now->children, children) {
-		printk("[ptree] %s,%d\n", t->comm, t->pid);
+	struct prinfo info;
+	pid_t ns = 0;
+	printk("[ptree] %s,%d\n", now->comm, now->pid);
+	
+	if (now != &init_task){
+		re = 1;
+		strcpy(info.comm, now->comm);
+		if (list_empty(&now->children)) info.first_child_pid = 0;
+		else info.first_child_pid = list_entry(now->children.next, struct task_struct, sibling)->pid;
+		info.next_sibling_pid = next_sibling;
+		info.parent_pid = par;
+		info.pid = now->pid;
+		info.state = now->state;
+		info.uid = task_uid(now);
+		if(*out != end){
+			err = copy_to_user(&info, *out, sizeof(struct prinfo));
+			if(err < 0) return err;
+			(*out)++;
+		}
 	}
-	*/
+
 	list_for_each(p, &now->children) {
 		t = list_entry(p, struct task_struct, sibling);
-		//if(dep == 0) dfs(t, dep+1);
-		if(t != NULL) dfs(t, dep+1);
+		ns = 0;
+		if(p->next != &(now->children)) {
+			ns = list_entry(p->next, struct task_struct, sibling)->pid;
+		}
+		re += ptree_dfs(t, out, end, ns, now->pid);
 	}
+	return re;
 }
+
+
 SYSCALL_DEFINE2(ptree, struct prinfo *, buf, int *, nr)
 {
-	struct task_struct *root = &init_task;
-	printk("[ptree] %d\n", root->pid);
-	printk("[ptree] hello, world!\n");
-	dfs(root, 0);
-	return 1;
+	int list_size;
+	int err = 0;
+	int num_of_proc;
+
+	if (buf == NULL || nr == NULL) return -EINVAL;
+
+	if ((err = get_user(list_size, nr)) < 0) return err;
+	if (list_size <= 0) return -EINVAL;
+
+	if (access_ok(VERIFY_WRITE, buf, list_size * sizeof(struct prinfo)) < 0) return -EFAULT;
+
+	read_lock(&tasklist_lock);
+	num_of_proc = ptree_dfs(&init_task, &buf, buf + list_size, -1, -1);
+	read_unlock(&tasklist_lock);
+
+	if (num_of_proc < 0) return num_of_proc;
+	return num_of_proc - 1; 
 }
