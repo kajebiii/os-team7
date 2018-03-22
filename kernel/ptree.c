3012cab6e72e4bf906a16dd748bf6bf60b297df0
Lee Jaebeom <woqja125@gmail.com>
2018-03-23 00:53:48 +0900

    change many thing

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -7,36 +7,34 @@
 #include <linux/string.h>
 #include <linux/slab.h>
 
-static int ptree_dfs(struct task_struct *now, struct prinfo* out, int *idx, int max_len, pid_t next_sibling, pid_t par) {
-	int re = 0, err;
+static int get_next_sibling(struct task_struct *now){
+	if(now->sibling.next == &(now->real_parent->children)) return 0;
+	return list_entry(now->sibling.next, struct task_struct, sibling)->pid;
+}
+
+static int ptree_dfs(struct task_struct *now, struct prinfo* out, struct prinfo *end) {
+	int re = 0;
 	struct list_head *p;
 	struct task_struct *t;
-	pid_t ns = 0;
-	printk("[ptree] %s,%d\n", now->comm, now->pid);
 	
 	if (now != &init_task){
 		re = 1;
-		if(*idx < max_len){
-			strcpy(out[*idx].comm, now->comm);
-			if (list_empty(&now->children)) out[*idx].first_child_pid = 0;
-			else out[*idx].first_child_pid = list_entry(now->children.next, struct task_struct, sibling)->pid;
-			out[*idx].next_sibling_pid = next_sibling;
-			out[*idx].parent_pid = par;
-			out[*idx].pid = now->pid;
-			out[*idx].state = now->state;
-			out[*idx].uid = task_uid(now);
-			(*idx)++;
+		if(out < end){
+			strcpy(out->comm, now->comm);
+			if (list_empty(&now->children)) out->first_child_pid = 0;
+			else out->first_child_pid = list_entry(now->children.next, struct task_struct, sibling)->pid;
+			out->next_sibling_pid = get_next_sibling(now);
+			out->parent_pid = now->real_parent->pid;
+			out->pid = now->pid;
+			out->state = now->state;
+			out->uid = task_uid(now);
 		}
 		else printk("[ptree]    end of buffer\n");
 	}
 
 	list_for_each(p, &now->children) {
 		t = list_entry(p, struct task_struct, sibling);
-		ns = 0;
-		if(p->next != &(now->children)) {
-			ns = list_entry(p->next, struct task_struct, sibling)->pid;
-		}
-		re += ptree_dfs(t, out, idx, max_len, ns, now->pid);
+		re += ptree_dfs(t, out+re, end);
 	}
 	return re;
 }
@@ -47,9 +45,8 @@ SYSCALL_DEFINE2(ptree, struct prinfo *, buf, int *, nr)
 	int list_size;
 	int err = 0;
 	int num_of_proc;
-	int remain_bytes, write_bytes;
+	int remain_bytes, return_value;
 	void *kbuf, *ubuf, *kbuf_origin;
-	int idx = 0;
 
 	if (buf == NULL || nr == NULL) return -EINVAL;
 
@@ -59,28 +56,25 @@ SYSCALL_DEFINE2(ptree, struct prinfo *, buf, int *, nr)
 	if (access_ok(VERIFY_WRITE, buf, list_size * sizeof(struct prinfo)) == 0) return -EFAULT;
 
 	kbuf_origin = kbuf = kmalloc(list_size*sizeof(struct prinfo), GFP_KERNEL);
-
 	if (kbuf == NULL) return -ENOSPC;
+	
 	read_lock(&tasklist_lock);
-	num_of_proc = ptree_dfs(&init_task, kbuf, &idx, list_size, -1, -1);
+	num_of_proc = ptree_dfs(&init_task, kbuf, kbuf + list_size * sizeof(struct prinfo));
 	read_unlock(&tasklist_lock);
 
 	if (num_of_proc < 0) return num_of_proc;
+	if (list_size > num_of_proc) list_size = num_of_proc;
 
-	if(list_size > num_of_proc) list_size = num_of_proc;
 	ubuf = buf;
 	remain_bytes = list_size * sizeof(struct prinfo);
-	while(remain_bytes > 0){
-		write_bytes = copy_to_user(ubuf, kbuf, remain_bytes);
-		if(write_bytes < 0) return write_bytes;
-		write_bytes = remain_bytes - write_bytes;
-		ubuf += write_bytes;
-		kbuf += write_bytes;
-		remain_bytes -= write_bytes;
-	}
-	if((err = put_user(list_size, nr)) < 0) return err;
+	return_value = num_of_proc;
+
+	kbuf += remain_bytes; ubuf += remain_bytes;
+	while((remain_bytes = copy_to_user(ubuf - remain_bytes, kbuf - remain_bytes, remain_bytes)) > 0);
+
+	if(remain_bytes < 0) return_value = remain_bytes;
+	if((err = put_user(list_size, nr)) < 0) return_value = err;
 
 	kfree(kbuf_origin);
-
-	return num_of_proc;
+	return return_value;
 }
--- test/test_ptree.c
+++ test/test_ptree.c
@@ -1,26 +1,28 @@
 #include <asm/unistd.h>
 #include <errno.h>
 #include <stdio.h>
-#include <memory.h>
-#include <linux/prinfo.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+struct prinfo {
+  long state;             /* current state of process */
+  pid_t pid;              /* process id */
+  pid_t parent_pid;       /* process id of parent */
+  pid_t first_child_pid;  /* pid of oldest child */
+  pid_t next_sibling_pid; /* pid of younger sibling */
+  long uid;               /* user id of process owner */
+  char comm[64];          /* name of program executed */
+};
+
 
 int main() {
 	struct prinfo *data = NULL;
 	int size = 500, re, i;
 	int *stk, top=-1;
-	while(true){
-//		printf("I will malloc !!!\n");		
+	while(1){		
 		data = (struct prinfo*)malloc(size * sizeof(struct prinfo));
 		re = syscall(380, data, &size);
-
-//		printf("^^Hi %d %d\n", size, re);
-//	for(i=0; i < re && i < size; i++){
-//		printf("^^%*s%s,%d,%ld,%d,%d,%d,%d\n", 0, "", data[i].comm, data[i].pid,
-//			data[i].state,data[i].parent_pid, data[i].first_child_pid, data[i].next_sibling_pid, data[i].uid);
-//	}
-
-//		printf("^^Hello %d %d\n", size, re);
-		if(re <= size) break;
+		if(re == size) break;
 		size = re + 10;
 		free(data);
 	}
