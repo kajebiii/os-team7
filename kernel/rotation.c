a7b8646bab2ed2e4e35cc0527c7ad2e7a3fc5f0b
Lee Jaebeom <woqja125@gmail.com>
2018-04-13 18:36:22 +0900

    make skeleton and TODO comments

--- kernel/rotation.c
+++ kernel/rotation.c
@@ -3,7 +3,7 @@
 #include <linux/syscalls.h>
 #include <linux/sched.h>
 #include <linux/mutex.h>
-#include <linux/semaphore.h>
+#include <linux/completion.h>
 #include <linux/list.h>
 
 int SYSTEM_DEGREE = 0;
@@ -17,7 +17,7 @@ struct lock_info{
 }
 
 struct wait_node{
-	struct semaphore sem;
+	struct completion comp;
 	struct lock_info* lockinfo;
 	struct list_head list;
 };
@@ -28,21 +28,37 @@ static LIST_HEAD(wait_node_list_write);
 struct lock_info* write_accquired[360];
 struct list_head read_accquired[360];
 
+/*
+	ISSUE
+	mutex to spinlock?
+	call find_avaialbe in lock?
+*/
+
 static void find_available(){
 	wait_node *iter;
+	int i;
 
 	mutex_lock(rotlock_mutex);
 
 	list_for_each_entry(iter, &wait_node_list_write, list){
 		// TODO: check for curret iter
-
+		// for i in range check
+		if(true /* able to get lock */){
+			// TODO : edit write_acc
+			// remove from wait_node_write
+			complete(&(iter->comp));
+			mutex_unlock(rotlock_mutex);
+			return;
+		}
 	}
 
+	// DO simillar for read
 
-	mutex_unlock(rotlock_mutext);
+	mutex_unlock(rotlock_mutex);
 }
 
 static int lock(int degree, int range, int mode){
+	int err;
 	// TODO : validate degree, range
 
 	// make waitnode struct
@@ -52,34 +68,57 @@ static int lock(int degree, int range, int mode){
 	mynode->lockinfo->range = range;
 	mynode->lockinfo->mode = mode;
 	mynode->lockinfo->proc = currunt;
-	sema_init(&(mynode->sem), 0);
+	init_completion(&(mynode->comp));
 
 	// accquire lock
 	mutex_lock(rotlock_mutex);
 	list_add(&(wait_node->list), mode==1?wait_node_list_read.prev:wait_node_list_write.prev);
 	mutex_unlock(rotlock_mutex);
-
+// ??? can wrong thread wakeup occurs
 	find_available();
+
+	err = wait_for_completion_interruptible(&(mynode->comp));
+	if(err == -ERESTARTSYS){
+		// TODO : for return erestart, setup restart struct (maybe move wait to another function)
+		return -ERESTARTSYS
+	} 
+	// else err == 0, get lock and return
+	return 0;
 }
 
 SYSCALL_DEFINE2(rotunlock_read, int, degree, int, range)
 {
-	return 383;
+	// TODO: validate degree, range
+	// TODO: check this unlock exist in accuire
+	// TODO: if so, remove from acc
+	find_available();
+	return 0;
 }
 
 SYSCALL_DEFINE2(rotunlock_write, int, degree, int, range)
 {
-	return 385;
+	// TODO: validate degree, range
+	// TODO: check this unlock exist in accuire
+	// TODO: if so, remove from acc
+	find_available();
+	return 0;
 }
 
 SYSCALL_DEFINE1(set_rotation, int, degree)
 {
-	return 380;
+	mutex_lock(rotlock_mutex);
+	SYSTEM_DEGREE = degree;
+	mutex_unlock(rotlock_mutex);
+	find_available();
+	return 0;
 }
 
 void exit_rotlock(struct task_struct *tsk)
 {
-	//
+	mutex_lock();
+	// find all accquired or waiting lock with tsk and remove
+	mutex_unlock();
+	find_available();
 }
 
 // init array of list_head
