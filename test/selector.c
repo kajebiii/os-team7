c143bb07035548ae9fbafa93cea4b7097b7dd1f8
JongBeom Kim <kajebiii@gmail.com>
2018-04-14 13:03:10 +0900

    modify selector.c trial.c

--- test/selector.c
+++ test/selector.c
@@ -1,18 +1,39 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<time.h>
-void delay(clock_t n){
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <time.h>
+
+#define SYSCALL_SET_ROTATION 380
+#define SYSCALL_ROTLOCK_READ 381
+#define SYSCALL_ROTLOCK_WRITE 382
+#define SYSCALL_ROTUNLOCK_READ 383
+#define SYSCALL_ROTUNLOCK_WRTIE 385
+
+void delay(int sec){
     clock_t start = clock();
-    while(clock() - start < n);
+    while((clock() - start) / CLOCKS_PER_SEC < sec);
 }
 int main(int argc, char *argv[]){
+	if(argc != 2) {
+		printf("Usage ./selector [STARTING INTEGER]");
+		return 0;
+	}
     int n = atoi(argv[1]);
+	if(n <= 1) {
+		printf("[STARTING INTEGER] must be large than or equal to 2");
+		return 0;
+	}
     while(1){
-        FILE *fp = fopen("input","w");
-        printf("%d\n",n);
+        FILE *fp = fopen("integer", "w");
+		//syscall(SYSCALL_ROTLOCK_WRITE, 90, 90);
         fprintf(fp, "%d\n",n);
+        printf("selector: %d\n",n);
+		fclose(fp);
+		//syscall(SYSCALL_ROTUNLOCK_WRITE, 90, 90);
         n++;
-        delay(1000);
+        delay(1);
     }
 }
--- test/trial.c
+++ test/trial.c
@@ -1,25 +1,48 @@
-#include<stdio.h>
-#include<time.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <time.h>
 
-void delay(clock_t n){
+#define SYSCALL_SET_ROTATION 380
+#define SYSCALL_ROTLOCK_READ 381
+#define SYSCALL_ROTLOCK_WRITE 382
+#define SYSCALL_ROTUNLOCK_READ 383
+#define SYSCALL_ROTUNLOCK_WRTIE 385
+
+void delay(int sec){
     clock_t start = clock();
-    while(clock() - start < n);
+    while((clock() - start) / CLOCKS_PER_SEC < sec);
 }
 
 int main(int argc, char *argv[]){
+	if(argc != 2) {
+		printf("Usage ./selector [INTEGER IDENTIFIER]");
+		return 0;
+	}
+    int id = atoi(argv[1]);
+	if(id <= -1) {
+		printf("[INTEGER IDENTIFIER] must be large than or equal to 0");
+		return 0;
+	}
     while(1){
         int n, i;
-        FILE *fp = fopen("integer","r");
-        fscanf(fp, "%d",&n);
-        for(i=2;i<=n;i++){
-            while(n%i==0){
-                n/=i;
-                printf("%d",i);
-                if(n!=1){
-                    printf(" * ");
-                }
+        FILE *fp = fopen("integer", "r");
+		//syscall(SYSCALL_ROTLOCK_READ, 90, 90);
+        fscanf(fp, "%d", &n);
+		printf("trial-%d: %d = ", id, n);
+        for(i=2; i<=n; i++){
+            while(n % i == 0){
+                n /= i;
+                printf("%d", i);
+                if(n != 1) printf(" * ");
             }
         }
-        delay(1000);
+		puts("");
+		fclose(fp);
+		//syscall(SYSCALL_ROTLOCK_READ, 90, 90);
+        delay(1);
     }
 }
