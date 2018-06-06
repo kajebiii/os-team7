eb3fb6f93ce744af9a1bba1169befdd44a83bb1e
Lee Jaebeom <woqja125@gmail.com>
2018-06-07 05:37:22 +0900

    prepare get_gps_location syscall

--- kernel/gps.c
+++ kernel/gps.c
@@ -1,3 +1,4 @@
+
 #include <linux/syscalls.h>
 #include <linux/gps.h>
 
@@ -5,4 +6,8 @@ struct gps_location curret_location = {0, 0, 0, 0, 0};
 
 SYSCALL_DEFINE1(set_gps_location, struct gps_location __user *, loc) {
     return copy_from_user(&current_location, loc, sizeof(struct gps_location));
+}
+
+SYSCALL_DEFINE2(get_gps_location, const char __user *, pathname, struct gps_location __user *, loc) {
+    return -EINVAL;
 }
