63d20f0ee525663b83b9dc1c8712bda4b1014d2f
Lee Jaebeom <woqja125@gmail.com>
2018-06-07 07:53:29 +0900

    add comments; TODO lock

--- kernel/gps.c
+++ kernel/gps.c
@@ -2,6 +2,8 @@
 #include <linux/syscalls.h>
 #include <linux/gps.h>
 
+// TODO: lock will need;
+
 struct gps_location current_location = {0, 0, 0, 0, 0};
 
 SYSCALL_DEFINE1(set_gps_location, struct gps_location __user *, loc) {
