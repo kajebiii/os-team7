1d6c94575f0fa1d573019c34f9fb135e46cc38e0
Lee Jaebeom <woqja125@gmail.com>
2018-06-07 08:50:31 +0900

    prepare permission check

--- fs/ext2/ext2.h
+++ fs/ext2/ext2.h
@@ -768,6 +768,7 @@ extern int ext2_fiemap(struct inode *inode, struct fiemap_extent_info *fieinfo,
 		       u64 start, u64 len);
 extern int ext2_set_gps_location(struct inode *);
 extern int ext2_get_gps_location(struct inode *, struct gps_location *);
+extern int ext2_permission (struct inode *, int);
 
 /* ioctl.c */
 extern long ext2_ioctl(struct file *, unsigned int, unsigned long);
