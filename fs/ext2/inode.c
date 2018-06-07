07e2303396a4f33abd55a8aa559423f0a3098292
Lee Jaebeom <woqja125@gmail.com>
2018-06-07 09:05:19 +0900

    edit permission

--- fs/ext2/inode.c
+++ fs/ext2/inode.c
@@ -1600,49 +1600,13 @@ int ext2_get_gps_location(struct inode *inode, struct gps_location *loc) {
 	return 0;
 }
 
+extern int generic_permission(struct inode *, int);
+
 int ext2_permission(struct inode *inode, int mask) {
-	int ret;
 	struct ext2_inode_info *ei = EXT2_I(inode);
 
 	// TODO: check geo_permission here
 
-
-	// below is copy of generic_permission
-
-	/*
-	 * Do the basic permission checks.
-	 */
-	ret = acl_permission_check(inode, mask);
-	if (ret != -EACCES)
-		return ret;
-
-	if (S_ISDIR(inode->i_mode)) {
-		/* DACs are overridable for directories */
-		if (capable_wrt_inode_uidgid(inode, CAP_DAC_OVERRIDE))
-			return 0;
-		if (!(mask & MAY_WRITE))
-			if (capable_wrt_inode_uidgid(inode,
-						     CAP_DAC_READ_SEARCH))
-				return 0;
-		return -EACCES;
-	}
-	/*
-	 * Read/write DACs are always overridable.
-	 * Executable DACs are overridable when there is
-	 * at least one exec bit set.
-	 */
-	if (!(mask & MAY_EXEC) || (inode->i_mode & S_IXUGO))
-		if (capable_wrt_inode_uidgid(inode, CAP_DAC_OVERRIDE))
-			return 0;
-
-	/*
-	 * Searching includes executable on directories, else just read.
-	 */
-	mask &= MAY_READ | MAY_WRITE | MAY_EXEC;
-	if (mask == MAY_READ)
-		if (capable_wrt_inode_uidgid(inode, CAP_DAC_READ_SEARCH))
-			return 0;
-
-	return -EACCES;
+	return generic_permission(inode, mask);
 }
 
